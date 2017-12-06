0046fd7f7f3b2a35961339b7cd229d4c7e6ef068
Mohaiyo <Mohaiyo@163.com>
2017-12-06 23:45:25 +0800

    modify index.vue

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,11 +1,59 @@
 <template>
-	<div>
-		<div>
-			<div>
-				底部内容
-			</div>
-		</div>
-	</div>
+    <div class="content">
+        <div class="footer">
+        	<div class="inner">
+        		<div class="left">
+        			<div class="l">
+        				<div class="logo"><img src="/src/assets/images/foot-logo.png" alt=""></div>
+        				<p>联系我们：<br><span>0571-5823 9809</span></p>
+        				<p>会员QQ群：643152314</p>
+        				<p>微信客服：mayiforex</p>
+        			</div>
+        			<div class="c">
+        				<p>蚂蚁外汇IOS下载</p>
+        				<img src="/src/assets/images/QRcode-IOS.jpg" alt="">
+        			</div>
+        			<div class="s">
+        				<p>蚂蚁外汇安卓下载</p>
+        				<img src="/src/assets/images/QRcode-IOS.jpg" alt="">
+        			</div>
+        			<div class="b">Copyright © 2017 杭州喜财网络科技有限公司 版权所有 ｜ 浙ICP备17022232号</div>
+        		</div>
+        		<div class="right">
+        			<div class="a"><h1>APP功能介绍</h1></div>
+        			<div class="a" style="float: right;">
+        				<h1>关于我们</h1>
+        				<a href="about2.html">蚂蚁外汇介绍</a>
+        				<a>STP交易模式</a>
+        				<a>常见问题</a>
+        			</div>
+        			<div class="a" style="float: right;">
+        				<h1>半小时学外汇</h1>
+        				<a href="#">外汇交易的优势</a>
+        				<a href="#">基础知识</a>
+        				<a href="#">如何阅读图表</a>
+        				<a href="#">交易示例</a>
+        				<a href="#">交易外汇最佳时间</a>
+        				<a href="#">从蚂蚁外汇开始交易</a>
+        				<a >来自专业人事的十大建议</a>
+        			</div>
+        			<div class="a">
+        				<h1>交易品种</h1>
+        				<a href="#">外汇</a>
+        				<a href="#">原油</a>
+        				<a href="#">贵金属</a>
+        				<a href="#">CFD</a>
+        			</div>
+        			<div class="a" style="float: right;    margin-top: -140px;">
+        				<h1>风险提示</h1>
+        				<a>外汇投资风险提示</a>
+        				<a>投资者风险披露</a>
+        			</div>
+        			<p>分享到：<i class="wechat"></i><i class="QQ"></i><i class="sina"></i></p>
+        		</div>
+        	</div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
@@ -17,3 +65,12 @@ export default {
   }
 }
 </script>
+<style scoped>
+    .content {
+        position: relative;
+        width: 1050px;
+        height: 400px;
+        padding-top: 0px;
+        margin: 0 auto;
+    }
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -5,42 +5,6 @@
 		</div>
 
 		<div class="content">
-<!-- 			<div class="swiper-container" >
-				<div class="swiper-wrapper">
-					<div class="swiper-slide slide1">
-						<h1>门槛极低</h1>
-						<span class="line2"></span>
-						<p>开户就送200美元体验金，最低10美元交易一手</p>
-					</div>
-					<div class="swiper-slide slide2">
-						<h1>高收益</h1>
-						<span class="line2"></span>
-						<p>涨跌皆有机会赚钱，多倍杠杆，200 美金可以当作2000 美金来投资赚钱。</p>
-					</div>
-					<div class="swiper-slide slide3">
-						<h1>顶级券商</h1>
-						<span class="line2"></span>
-						<p>蚂蚁外汇与国际知名券商合作，国际市场，每天5万亿以上成交量。</p>
-					</div>
-					<div class="swiper-slide slide4">
-						<h1>操作简单</h1>
-						<span class="line2"></span>
-						<p>操作简易，新手一看就会，菜鸟轻松上路。</p>
-					</div>
-					<div class="swiper-slide slide5">
-						<h1>机会多</h1>
-						<span class="line2"></span>
-						<p>24小时交易，T＋0交易，大小行情不错过。</p>
-					</div>
-					<div class="swiper-slide slide6">
-						<h1>跟随高手</h1>
-						<span class="line2"></span>
-						<p>无需精通理财知识，跟着高手轻松赚大钱。</p>
-					</div>
-				</div>
-			</div>
-			<div class="swiper-button-prev"></div>
-			<div class="swiper-button-next"></div> -->
       <div class="swiper-container">
         <swiper :options="swiperOption" ref="mySwiper" class="swiper-wrapper">
           <swiper-slide class="swiper-slide slide1">
@@ -75,11 +39,11 @@
           </swiper-slide>
           <!-- Optional controls -->
           <!-- <div class="swiper-pagination"  slot="pagination"></div> -->
-          <div class="swiper-button-prev" slot="button-prev"></div>
-          <div class="swiper-button-next" slot="button-next"></div>
           <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
         </swiper>
       </div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-button-next"></div>
 		</div>
 	</div>
 </template>
@@ -91,6 +55,15 @@ export default {
   data () {
     return {
       swiperOption: {
+        direction: 'horizontal',
+        loop: false,
+        slidesPerView: 3,
+        spaceBetween: 30,
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
         // some swiper options/callbacks
         // 所有的参数同 swiper 官方 api 参数
         // ...
@@ -106,7 +79,7 @@ export default {
     // current swiper instance
     // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
     console.log('this is current swiper instance object', this.swiper)
-    this.swiper.slideTo(3, 1000, false)
+    // this.swiper.slideTo(3, 1000, false)
   },
   components: {
     swiper,
@@ -114,3 +87,10 @@ export default {
   }
 }
 </script>
+<style lang='scss' scoped>
+  .content{
+     .swiper-slide{
+        width: 330px !important;
+     }
+  }
+</style>
